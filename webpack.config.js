44ac6893832245f0e3eca942ad04a4268d78995d
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-07 19:09:14 -0300

    Add CSS Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,12 @@ module.exports = {
   },
   mode: 'development',
   plugins: [new HTMLWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
